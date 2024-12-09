18c451dc7c16e2b3c0fabc9cb6e19cb43af1dabf
soujanya <soujanya.u774@gmail.com>
2024-12-09 21:24:20 +0530

    new

--- sample.java
+++ sample.java
@@ -1,6 +1,8 @@
 class sample
   {
-    public static void main(String[],args)
+    public static void main(String[]  args)
     {
       System.out.println("vvce");
+      System.out.println("mysore");
+
     }}
